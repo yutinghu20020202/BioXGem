6378b4d1c148cd29f5aa5f0bdb7fff34301fea50
yuting <lily200202021@gmail.com>
2024-09-19 10:34:16 +0800

    0919hw-2

--- 程式作業/Dungeons&DragonsPart2.c
+++ 程式作業/Dungeons&DragonsPart2.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 int main () {
     int d1=0, d2=0, d3=0, d4=0, d5=0, d6=0, temp=0, playerScore=100, dragonsScore=100;
-    scanf("%d %d %d %d %d %d", &d1, &d2, &d3, &d4, &d5, &d6);
-
-    int sum123 = d1 + d2 + d3;
+    
     while(1){
         scanf("%d %d %d %d %d %d", &d1, &d2, &d3, &d4, &d5, &d6);
         int sum123 = d1 + d2 + d3;
+
         if(sum123%2 != 0){
             if (d6>d5 && d5>d4){
                 dragonsScore-=25;
             }else if(d4%2 == d5%2 || d5%2 == d6%2){
                 dragonsScore-=5;
             }else{
-                return 0;
+                continue;
             }
         }else{
-            if(d5-d4 > d6){
+            if(abs(d5-d4) > d6){
                 playerScore-=15;
             }else if(d4+d5 != d6){
                 playerScore-=5;
             }else{
-                return 0;
+                continue;
             }
         }
-        if(playerScore==0){
+        if(playerScore<=0){
             printf("Dragon wins\n");
             break;
-        }else if(dragonsScore==0){
+        }else if(dragonsScore<=0){
             printf("Player wins\n");
             break;
         }
